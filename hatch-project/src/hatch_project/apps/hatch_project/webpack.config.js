8fad67592b15df5ab76715e8c1b016f4747ecfa5
Willem van Heemstra <wvanheemstra@icloud.com>
2025-02-20 16:53:12 +0100

    refactor: Streamline webpack configuration and remove redundant plugins

--- hatch-project/src/hatch_project/apps/hatch_project/webpack.config.js
+++ hatch-project/src/hatch_project/apps/hatch_project/webpack.config.js
@@ -1,10 +1,6 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
-const { NxWebpackPlugin } = require('@nx/webpack');
 const path = require('path');
-
-// Get the absolute path to the project root
-const projectRoot = path.resolve(__dirname, '../../../../..');
 
 module.exports = {
   output: {
@@ -20,36 +16,25 @@ module.exports = {
   },
   plugins: [
     new NxAppWebpackPlugin({
-      tsConfig: './tsconfig.app.json',
+      assets: [
+        {
+          glob: 'src/favicon.ico',  // Relative to the app directory
+          input: __dirname,  // Current directory (where webpack.config.js is)
+          output: '.'
+        }
+      ],
+      tsConfig: './tsconfig.app.json',  // Relative path
       compiler: 'babel',
-      main: './src/main.tsx',
-      index: './src/index.html',
+      main: './src/main.tsx',  // Relative path
+      index: './src/index.html',  // Relative path
       baseHref: '/',
-      assets: ['./src/favicon.ico', './src/assets'],
-      styles: ['./src/styles.css'],
-      outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
-      optimization: process.env['NODE_ENV'] === 'production',
+      outputPath: 'dist/apps/hatch_project',
+      target: 'web'
     }),
     new NxReactWebpackPlugin({
       // Uncomment this line if you don't want to use SVGR
       // See: https://react-svgr.com/
       // svgr: false
     }),
-    new NxWebpackPlugin({
-      assets: [
-        {
-          glob: 'src/favicon.ico',  // Path relative to apps/hatch_project
-          input: path.resolve(__dirname),  // Current directory (apps/hatch_project)
-          output: '.'
-        }
-      ],
-      tsConfig: path.resolve(__dirname, 'tsconfig.app.json'),
-      compiler: 'babel',
-      main: path.resolve(__dirname, 'src/main.tsx'),
-      index: path.resolve(__dirname, 'src/index.html'),
-      baseHref: '/',
-      outputPath: 'dist/apps/hatch_project',
-      target: 'web'
-    })
-  ],
+  ]
 };
